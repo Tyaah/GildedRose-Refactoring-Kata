d595dcb1de05901d9fe01e10b68bd95621cc7869
Tyaah <114932070+Tyaah@users.noreply.github.com>
2026-02-03 22:00:03 +0100

    refactor: add quality and sellin management methods

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -22,48 +22,72 @@ export class GildedRose {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+            this.items[i] = this.reduceQualityByOneForItem(this.items[i]);
           }
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
+          this.items[i] = this.increaseQualityByOneForItem(this.items[i]);
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                this.items[i] = this.increaseQualityByOneForItem(this.items[i]);
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                this.items[i] = this.increaseQualityByOneForItem(this.items[i]);
               }
             }
           }
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.items[i] = this.reduceSellInByOneForItem(this.items[i]);
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
+                this.items[i] = this.reduceQualityByOneForItem(this.items[i]);
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+            this.items[i] = this.setQualityToZeroForItem(this.items[i]);
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
+            this.items[i] = this.increaseQualityByOneForItem(this.items[i]);
           }
         }
       }
     }
 
     return this.items;
+  }
+
+  private reduceSellInByOneForItem(item: Item):Item {
+    const newItem: Item = item;
+    newItem.sellIn = item.sellIn - 1;
+    return newItem;
+  }
+
+  private reduceQualityByOneForItem(item: Item):Item {
+    const newItem: Item = item;
+    newItem.quality = item.quality - 1;
+    return newItem;
+  }
+
+  private increaseQualityByOneForItem(item: Item):Item {
+    const newItem: Item = item;
+    newItem.quality = item.quality + 1;
+    return newItem;
+  }
+
+  private setQualityToZeroForItem(item: Item):Item {
+    const newItem: Item = item;
+    newItem.quality = 0;
+    return newItem;
   }
 }
