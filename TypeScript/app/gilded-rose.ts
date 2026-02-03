df149f6312c0142d0bbd7115a66681146a91afa1
Tyaah <114932070+Tyaah@users.noreply.github.com>
2026-02-03 22:25:46 +0100

    refactor: add docs to strategies and expand base methods

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -67,24 +67,69 @@ export class GildedRose {
     return this.items;
   }
 
+  /**
+   * Default strategy for updating an item
+   * It will:
+   * - Decrease sellIn by 1
+   * - Decrease quality by 1
+   * - If sellIn is less than 0, decrease quality by an additional 1
+   * @param item The item to update
+   * @returns The updated item
+   */
   private DefaultStrategy(item: Item):Item {
     let newItem = item;
     newItem = this.reduceSellInByOneForItem(newItem);
-    newItem = this.reduceQualityByOneForItem(newItem);
+
+    if (newItem.sellIn < 0) {
+      newItem = this.reduceQualityByTwoForItem(newItem);
+    } else {
+      newItem = this.reduceQualityByOneForItem(newItem);
+    }
     return newItem;
   }
 
+  /**
+   * Strategy for updating Aged Brie
+   * It will:
+   * - Decrease sellIn by 1
+   * - Increase quality by 1
+   * - If sellIn is less than 0, increase quality by an additional 1
+   * @param item The item to update
+   * @returns The updated item
+   */
   private AgedBrieStrategy(item: Item):Item {
     let newItem = item;
     newItem = this.reduceSellInByOneForItem(newItem);
-    newItem = this.increaseQualityByOneForItem(newItem);
+
+    if (newItem.sellIn < 0) {
+      newItem = this.increaseQualityByTwoForItem(newItem);
+    } else {
+      newItem = this.increaseQualityByOneForItem(newItem);
+    }
     return newItem;
   }
 
+  /**
+   * Strategy for updating Sulfuras
+   * Sulfuras does not change in quality or sellIn
+   * @param item The item to update
+   * @returns The updated item
+   */
   private SulfurasStrategy(item: Item):Item {
     return item;
   }
 
+  /**
+   * Strategy for updating Backstage Passes
+   * It will:
+   * - Decrease sellIn by 1
+   * - Increase quality by 1
+   * - If sellIn is 10 or less, increase quality by an additional 1
+   * - If sellIn is 5 or less, increase quality by an additional 1
+   * - If sellIn is less than 0, set quality to 0
+   * @param item The item to update
+   * @returns The updated item
+   */
   private BackstagePassesStrategy(item: Item):Item {
     let newItem = item;
     newItem = this.reduceSellInByOneForItem(newItem);
@@ -133,6 +178,25 @@ export class GildedRose {
   }
 
   /**
+   * Reduce quality by two for the given item
+   * Will not reduce quality below 0
+   * @param item The item to reduce quality for
+   * @returns The updated item with reduced quality
+   */
+  private reduceQualityByTwoForItem(item: Item):Item {
+    // Ensure quality does not go below 0
+    if (item.quality <= 1) {
+      const newItem: Item = item;
+      newItem.quality = 0;
+      return newItem;
+    }
+
+    const newItem: Item = item;
+    newItem.quality = item.quality - 2;
+    return newItem;
+  }
+
+  /**
    * Increase quality by one for the given item
    * Will not increase quality above 50
    * @param item The item to increase quality for
@@ -144,6 +208,25 @@ export class GildedRose {
 
     const newItem: Item = item;
     newItem.quality = item.quality + 1;
+    return newItem;
+  }
+
+  /**
+   * Increase quality by two for the given item
+   * Will not increase quality above 50
+   * @param item The item to increase quality for
+   * @returns The updated item with increased quality
+   */
+  private increaseQualityByTwoForItem(item: Item):Item {
+    // Ensure quality does not go above 50
+    if (item.quality >= 49) {
+      const newItem: Item = item;
+      newItem.quality = 50;
+      return newItem;
+    }
+
+    const newItem: Item = item;
+    newItem.quality = item.quality + 2;
     return newItem;
   }
 
