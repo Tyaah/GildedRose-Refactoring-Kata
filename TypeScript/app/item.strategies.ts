86d853787902d8de3601ef40b79478353865c06d
Tyaah <114932070+Tyaah@users.noreply.github.com>
2026-02-04 21:56:53 +0100

    refactor: apply utility methods to strategies remove legacy methods

--- TypeScript/app/item.strategies.ts
+++ TypeScript/app/item.strategies.ts
@@ -1,4 +1,5 @@
 import { Item } from './item.types';
+import { adjustQuality, adjustSellIn } from './item.utils';
 
 export class ItemStrategies {
   /**
@@ -11,13 +12,13 @@ export class ItemStrategies {
    * @returns The updated item
    */
   public DefaultStrategy(item: Item): Item {
-    let newItem = item;
-    newItem = this.reduceSellInByOneForItem(newItem);
+    let newItem = { ...item };
+    newItem = adjustSellIn(newItem, -1);
+    newItem = adjustQuality(newItem, -1);
 
+    // If sell date has passed, degrade quality again
     if (newItem.sellIn < 0) {
-      newItem = this.reduceQualityByTwoForItem(newItem);
-    } else {
-      newItem = this.reduceQualityByOneForItem(newItem);
+      newItem = adjustQuality(newItem, -1);
     }
     return newItem;
   }
@@ -32,14 +33,14 @@ export class ItemStrategies {
    * @returns The updated item
    */
   public AgedBrieStrategy(item: Item): Item {
-    let newItem = item;
-    newItem = this.reduceSellInByOneForItem(newItem);
+    let newItem = { ...item };
+    newItem = adjustSellIn(newItem, -1);
+    newItem = adjustQuality(newItem, 1);
 
+    // If sell date has passed, increase quality again
     if (newItem.sellIn < 0) {
-      newItem = this.increaseQualityByTwoForItem(newItem);
-    } else {
-      newItem = this.increaseQualityByOneForItem(newItem);
-    }
+      newItem = adjustQuality(newItem, 1);
+    } 
     return newItem;
   }
 
@@ -65,113 +66,24 @@ export class ItemStrategies {
    * @returns The updated item
    */
   public BackstagePassesStrategy(item: Item): Item {
-    let newItem = item;
-    newItem = this.reduceSellInByOneForItem(newItem);
+    let newItem = { ...item };
+    newItem = adjustSellIn(newItem, -1);
 
     // Sell date has passed, drop quality to 0
     if (newItem.sellIn < 0) {
-      newItem = this.setQualityToZeroForItem(newItem);
+      newItem = adjustQuality(newItem, -newItem.quality);
       return newItem;
     }
 
     // Increase quality based on remaining sellIn days
-    newItem = this.increaseQualityByOneForItem(newItem);
+    newItem = adjustQuality(newItem, 1);
     if (newItem.sellIn < 10) {
-      newItem = this.increaseQualityByOneForItem(newItem);
+      newItem = adjustQuality(newItem, 1);
     }
     if (newItem.sellIn < 5) {
-      newItem = this.increaseQualityByOneForItem(newItem);
+      newItem = adjustQuality(newItem, 1);
     }
     return newItem;
   }
 
-  /**
-   * Reduce sellIn by one for the given item
-   * @param item The item to reduce sellIn for
-   * @returns The updated item with reduced sellIn
-   */
-  private reduceSellInByOneForItem(item: Item): Item {
-    const newItem: Item = item;
-    newItem.sellIn = item.sellIn - 1;
-    return newItem;
-  }
-
-  /**
-   * Reduce quality by one for the given item
-   * Will not reduce quality below 0
-   * @param item The item to reduce quality for
-   * @returns The updated item with reduced quality
-   */
-  private reduceQualityByOneForItem(item: Item): Item {
-    // Ensure quality does not go below 0
-    if (item.quality <= 0) return item;
-
-    const newItem: Item = item;
-    newItem.quality = item.quality - 1;
-    return newItem;
-  }
-
-  /**
-   * Reduce quality by two for the given item
-   * Will not reduce quality below 0
-   * @param item The item to reduce quality for
-   * @returns The updated item with reduced quality
-   */
-  private reduceQualityByTwoForItem(item: Item): Item {
-    // Ensure quality does not go below 0
-    if (item.quality <= 1) {
-      const newItem: Item = item;
-      newItem.quality = 0;
-      return newItem;
-    }
-
-    const newItem: Item = item;
-    newItem.quality = item.quality - 2;
-    return newItem;
-  }
-
-  /**
-   * Increase quality by one for the given item
-   * Will not increase quality above 50
-   * @param item The item to increase quality for
-   * @returns The updated item with increased quality
-   */
-  private increaseQualityByOneForItem(item: Item): Item {
-    // Ensure quality does not go above 50
-    if (item.quality >= 50) return item;
-
-    const newItem: Item = item;
-    newItem.quality = item.quality + 1;
-    return newItem;
-  }
-
-  /**
-   * Increase quality by two for the given item
-   * Will not increase quality above 50
-   * @param item The item to increase quality for
-   * @returns The updated item with increased quality
-   */
-  private increaseQualityByTwoForItem(item: Item): Item {
-    // Ensure quality does not go above 50
-    if (item.quality >= 49) {
-      const newItem: Item = item;
-      newItem.quality = 50;
-      return newItem;
-    }
-
-    const newItem: Item = item;
-    newItem.quality = item.quality + 2;
-    return newItem;
-  }
-
-  /**
-   * Set quality to zero for the given item
-   * @param item The item to set quality to zero for
-   * @returns The updated item with quality set to zero
-   */
-  private setQualityToZeroForItem(item: Item): Item {
-    const newItem: Item = item;
-    newItem.quality = 0;
-    return newItem;
-  }
 }
