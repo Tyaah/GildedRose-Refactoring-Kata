2a0795c9dd9826d210fa65041fa5536157157541
Tyaah <114932070+Tyaah@users.noreply.github.com>
2026-02-04 21:57:08 +0100

    refactor: remove legacy updateQuality

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -11,56 +11,6 @@ export class GildedRose {
     this.strategies = new ItemStrategies();
   }
 
-  // updateQualityLegacy() {
-  //   for (let i = 0; i < this.items.length; i++) {
-  //     if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-  //       if (this.items[i].quality > 0) {
-  //         if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-  //           this.items[i] = this.reduceQualityByOneForItem(this.items[i]);
-  //         }
-  //       }
-  //     } else {
-  //       if (this.items[i].quality < 50) {
-  //         this.items[i] = this.increaseQualityByOneForItem(this.items[i]);
-  //         if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-  //           if (this.items[i].sellIn < 11) {
-  //             if (this.items[i].quality < 50) {
-  //               this.items[i] = this.increaseQualityByOneForItem(this.items[i]);
-  //             }
-  //           }
-  //           if (this.items[i].sellIn < 6) {
-  //             if (this.items[i].quality < 50) {
-  //               this.items[i] = this.increaseQualityByOneForItem(this.items[i]);
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-  //       this.items[i] = this.reduceSellInByOneForItem(this.items[i]);
-  //     }
-  //     if (this.items[i].sellIn < 0) {
-  //       if (this.items[i].name != 'Aged Brie') {
-  //         if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-  //           if (this.items[i].quality > 0) {
-  //             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-  //               this.items[i] = this.reduceQualityByOneForItem(this.items[i]);
-  //             }
-  //           }
-  //         } else {
-  //           this.items[i] = this.setQualityToZeroForItem(this.items[i]);
-  //         }
-  //       } else {
-  //         if (this.items[i].quality < 50) {
-  //           this.items[i] = this.increaseQualityByOneForItem(this.items[i]);
-  //         }
-  //       }
-  //     }
-  //   }
-
-  //   return this.items;
-  // }
-
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       switch (this.items[i].name) {
